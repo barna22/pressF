5e7c49a57f918bffa375e0a05e91beefb338b8f9
David <bartsch.david.a@gmail.com>
2020-03-29 22:02:22 +0200

    Van main :O

--- Szkeleton/src/szkeleton/Main.java
+++ Szkeleton/src/szkeleton/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main
 {
-	static void main(String[] args)
+	public static void main(String[] args)
 	{
 		String parancs;
 		Skeleton skeleton = new Skeleton();
