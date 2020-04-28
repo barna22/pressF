f12cd2eca174cf2295eea60811654d1aacd531d6
Lefayae <seres.adm@gmail.com>
2020-04-28 22:51:40 +0200

    lol

--- Proto/bin (Tesztre)/TestJar.ps1
+++ Proto/bin (Tesztre)/TestJar.ps1
@@ -2,7 +2,7 @@ function runtest([string]$infile, [string]$outfile)
 {
     $result = cat $outfile
     $input = cat $infile
-    $output = echo $input | java -cp . .\proto.Main
+    $output = echo $input | java -cp . proto.Main
 
     $diff = compare $output $result
 
