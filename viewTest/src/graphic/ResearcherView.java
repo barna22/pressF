019551e6fa8d5c1edd8a16d033540fbbcf82bcf7
barna22 <szubar10@gmail.com>
2020-05-18 16:09:46 +0200

    researcher active

--- viewTest/src/graphic/ResearcherView.java
+++ viewTest/src/graphic/ResearcherView.java
@@ -1,5 +1,7 @@
 package graphic;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -9,6 +11,7 @@ import javax.imageio.ImageIO;
 public class ResearcherView extends PlayerView{
 	private static BufferedImage researcher;
 	private static BufferedImage researcherinwater;
+	private static BufferedImage activeResearcher;
 	
 	public ResearcherView() {
 		try {
@@ -24,13 +27,26 @@ public class ResearcherView extends PlayerView{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		try {
+			if (activeResearcher == null)
+				activeResearcher = ImageIO.read(new File("images" + File.separator + "researcher scarf.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		Graphics g = activeResearcher.getGraphics();
+		g.setColor(Color.RED);
+		g.drawRect(0, 0, 31, 31);
+		g.dispose();
 	}
 	
 	public BufferedImage GetImage() {
-		if(player.isInWater) {
+		if(player.isInWater)
 			return researcherinwater;
-		}else {
+		else if (player.GetRemaningActions() > 0)
+			return activeResearcher;
+		else 
 			return researcher;
-		}
 	}
 }
